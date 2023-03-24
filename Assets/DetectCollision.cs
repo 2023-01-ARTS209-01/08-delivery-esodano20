aeb9c28fa4cbcfcfbc38a12ed58b61821260ff38
esodano20 <123187781+esodano20@users.noreply.github.com>
2023-03-24 03:01:25 -0400

    delivery and package sprites

--- Assets/DetectCollision.cs
+++ Assets/DetectCollision.cs
@@ -1,17 +1,63 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
+    // instance variales for the class
+
+    private bool hasPackage = false;
+
+    SpriteRenderer spriteRenderer;
+    [SerializeField]  Color hasPackageColor;
+    [SerializeField]  Color doesNotHavePackageColor;
+    [SerializeField] float driveSpeedup = 3.0f;
+    [SerializeField] float driveSpeeddown = 30.0f;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("We hit something!!! - " + collision.gameObject.name);
+        //Debug.Log("We hit something!!! - " + collision.gameObject.name);
+
+        if(collision.gameObject.CompareTag("Obsticle"))
+        {
+            Debug.Log("We hit something!!!");
+            float driveSpeeddown = 3.0f;
+
+        }
     }
 
     void OnTriggerEnter2D(Collider2D trigger)
     {
-        Debug.Log("We went through a TRIGGER!!! - " + trigger.gameObject.name);
+        //Debug.Log("We went through a TRIGGER!!! - " + trigger.gameObject.name);
+
+        if(trigger.gameObject.CompareTag("BoostPad"))
+        {
+            Debug.Log("Hit a boost pad wooo!!!");
+            float driveSpeedup = 30.0f;
+
+        }
+
+        if(trigger.gameObject.CompareTag("Package") && !hasPackage)
+        {
+            Debug.Log(">>>> Picked up packege!");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(trigger.gameObject, 0.5f);
+        }
+
+        if(trigger.gameObject.CompareTag("Customer") && hasPackage)
+        {
+            Debug.Log("<<<< Delivered the package!");
+            spriteRenderer.color = doesNotHavePackageColor;
+            hasPackage = false;
+        }
     }
-}
+}   
+    
 
